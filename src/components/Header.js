e7496867da1b9ad1bfb0bbe9213b682320b1431a
DulceBucio <114613877+DulceBucio@users.noreply.github.com>
2023-10-26 05:10:54 -0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Header.css"
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <h1>Portfolio</h1>
+        </header>
+    );
+}
