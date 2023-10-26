96086ce7838d90db3a2db16464152b61d2408c38
DulceBucio <dulcenahomibucio@gmail.com>
2023-10-26 13:21:30 -0600

    ADD header animations and Home contents

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,11 @@ import styles from "./Header.css"
 
 export const Header = () => {
     return (
-        <header className="header">
-            <h1>Portfolio</h1>
+        <header className="HeaderContainer">
+            <h1>Home</h1>
+            <h1>Projects</h1>
+            <h1>Experience</h1>
+            <h1>Contact</h1>
         </header>
     );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Home.css";
+
+export const Home = () => {
+    return(
+        <div className = "HomeContainer">
+            <h1>Dulce Bucio</h1>
+            <h2>Computer Science Student and STEM passionate</h2>
+            <p>I am a freshman Computer Science student at Tecnológico de Monterrey, driven to constantly learn and improve by embracing challenges and overcoming adversities. My passion for my career is immense, and I am fully committed to not only mastering my abilities but also deriving enjoyment from the journey while I share STEM knowledge to the world.</p>
+        </div>
+    )
+}
